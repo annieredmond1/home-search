118e69c38ac94fc7ee51a179e92b9ec6cb62f0c6
Annie Redmond <annieredmond1@gmail.com>
2017-07-04 13:00:45 -0700

    initial commit

---
+++ public/app.js
@@ -0,0 +1,9 @@
+'use strict'
+var homeSearch = angular.module('homeSearch', [
+
+])
+
+homeSearch.controller('searchController', function() {
+  var vm = this;
+  vm.title = "Home Search"
+})
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'index.html'));
+})
+
+app.listen(3000, 'localhost', function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Listening at http://localhost:3000');
+});
