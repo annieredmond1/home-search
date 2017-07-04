be88cbdecb0ed9178796563bef067b7f60df2904
Annie Redmond <annieredmond1@gmail.com>
2017-07-04 16:04:44 -0700

    parse csv and return results to front end

--- public/app.js
+++ public/app.js
@@ -3,13 +3,36 @@ var homeSearch = angular.module('homeSearch', [
 
 ])
 
-homeSearch.controller('searchController', function() {
+homeSearch.controller('searchController', ['homesService', function(homesService) {
   var vm = this;
   vm.queryTerm;
   vm.getResults = getResults;
 
   function getResults() {
     vm.pendingState = true;
-    console.log('searching for ', vm.queryTerm);
+    homesService.getSearchResults(vm.queryTerm)
+      .then(function(response) {
+        console.log(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+      .finally(function(response) {
+        vm.pendingState = false;
+      })
   }
-})
+}])
+
+homeSearch.factory('homesService', ['$http', function($http) {
+  return {
+    getSearchResults: getSearchResults
+  }
+
+  function getSearchResults(term) {
+    return $http({
+      url: '/api/homes',
+      method: 'GET',
+      params: {term: term}
+    });
+  }
+}])
--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+var _ = require('lodash');
+var csv = require('fast-csv');
 var express = require('express');
 var path = require('path');
+
 var app = express();
 
-
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + 'index.html'));
+})
+
+app.get('/api/homes', function(req, res) {
+  var homes = [];
+  csv
+    .fromPath('redfin_2017-07-02-15-47-37.csv', {headers: true})
+    .validate(function(data) {
+      return _.includes(data.ADDRESS.toLowerCase(), req.query.term.toLowerCase()) ||
+        _.includes(data.LOCATION.toLowerCase(), req.query.term.toLowerCase()) ||
+        _.includes([data.CITY.toLowerCase(), data.STATE.toLowerCase(), data.ZIP], req.query.term.toLowerCase());
+    })
+    .on('data', function(data) {
+      homes.push(data);
+    })
+    .on('end', function() {
+      res.send(homes);
+    })
+
+
 })
 
 app.listen(3000, 'localhost', function(err) {
